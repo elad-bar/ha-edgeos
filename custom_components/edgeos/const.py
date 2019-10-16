1a19f4198d5ce3174005ef3a58237e939f35a862
Elad Bar <elad.bar@hotmail.com>
2019-10-16 06:51:16 +0300

    Connection termination under Hass.io fixed

--- custom_components/edgeos/const.py
+++ custom_components/edgeos/const.py
@@ -180,4 +180,4 @@ MIN_TIME_BETWEEN_SCANS = timedelta(seconds=30)
 CONF_SUPPORTED_DEVICES = 'supported_devices'
 ATTR_ENABLED = 'enabled'
 
-ERROR_SHUTDOWN = "cannot schedule new futures after shutdown"
+ERROR_SHUTDOWN = "Connector is closed."
